939b8902727615e1684a0d7cbaa83907011ca737
TreehouseRestaurant <vegantreehouserestaurant@gmail.com>
2025-05-21 17:31:05 -0600

    Add files via upload

--- responsive.js
+++ responsive.js
@@ -19,3 +19,28 @@ questions.forEach(question => {
         question.classList.toggle('active');
     });
 });
+
+const foodBtn = document.getElementById('foodBtn');
+const drinksBtn = document.getElementById('drinksBtn');
+const snacksBtn = document.getElementById('snacksBtn');
+
+const foodMenu = document.getElementById('foodMenu');
+const drinksMenu = document.getElementById('drinksMenu');
+const snacksMenu = document.getElementById('snacksMenu');
+
+function showMenu(menuToShow) {
+  foodMenu.style.display = 'none';
+  drinksMenu.style.display = 'none';
+  snacksMenu.style.display = 'none';
+
+  foodBtn.classList.remove('active');
+  drinksBtn.classList.remove('active');
+  snacksBtn.classList.remove('active');
+
+  menuToShow.menu.style.display = 'block';
+  menuToShow.button.classList.add('active');
+}
+
+foodBtn.addEventListener('click', () => showMenu({ menu: foodMenu, button: foodBtn }));
+drinksBtn.addEventListener('click', () => showMenu({ menu: drinksMenu, button: drinksBtn }));
+snacksBtn.addEventListener('click', () => showMenu({ menu: snacksMenu, button: snacksBtn }));
